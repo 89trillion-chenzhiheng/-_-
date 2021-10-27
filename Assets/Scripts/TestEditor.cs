1fa5eec05feef10f8decf7d66335709fd0bbad7d
89trillion-chenzhiheng <93114635+89trillion-chenzhiheng@users.noreply.github.com>
2021-10-27 12:47:00 +0800

    Add files via upload

---
+++ Assets/Scripts/TestEditor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Test))]
+public class TestEditor : Editor
+{
+    public SerializedObject Obj;
+    public SerializedProperty Type;
+    public SerializedProperty TestMono;
+    public SerializedProperty Str;
+    public SerializedProperty Color;
+
+
+    private void OnEnable()
+    {
+        Obj = new SerializedObject(target);
+        Type = Obj.FindProperty("type");
+        TestMono = Obj.FindProperty("test");
+        Str = Obj.FindProperty("str");
+        Color = Obj.FindProperty("color");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        //Obj.Update();
+
+        EditorGUILayout.PropertyField(Type);
+        if(Type.enumValueIndex == 0)
+        {
+            EditorGUILayout.PropertyField(TestMono);
+        }
+        else if(Type.enumValueIndex == 1)
+        {
+            EditorGUILayout.PropertyField(Str);
+        }
+        else if(Type.enumValueIndex == 2)
+        {
+            EditorGUILayout.PropertyField(Color);
+        }
+
+
+        Obj.ApplyModifiedProperties();
+    }
+}
