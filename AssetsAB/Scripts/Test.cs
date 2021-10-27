0ff2fc6a59c825f3879e99a9259be4e9dfd8f7a5
89trillion-chenzhiheng <93114635+89trillion-chenzhiheng@users.noreply.github.com>
2021-10-27 12:55:10 +0800

    Add files via upload

---
+++ AssetsAB/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Type
+{
+    TestMono,
+    String,
+    Color,
+}
+
+
+public class Test : MonoBehaviour
+{
+    [HideInInspector]
+    public Type type;
+    [HideInInspector]
+    public Test test;
+    [HideInInspector]
+    public string str;
+    [HideInInspector]
+    public Color color;
+}
