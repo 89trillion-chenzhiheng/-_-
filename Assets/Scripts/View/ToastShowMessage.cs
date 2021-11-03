5d12b3ea17646b99b21e4516d77ecbb2ac844dae
chenzhiheng <chenzhiheng@89trillion.com>
2021-11-03 16:51:35 +0800

    排行榜

---
+++ Assets/Scripts/View/ToastShowMessage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToastShowMessage : MonoBehaviour
+{
+    // 显示角色名字
+    public Text userShowText;
+    // 显示角色奖杯数量
+    public Text rankShowText;
+
+    /// <summary>
+    /// 打开界面
+    /// </summary>
+    /// <param name="user">角色名字</param>
+    /// <param name="rank">角色奖杯数</param>
+    public void Show(string user, string rank)
+    {
+        userShowText.text = user;
+        rankShowText.text = rank;
+        gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// 关闭界面
+    /// </summary>
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
