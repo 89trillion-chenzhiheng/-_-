b21807c40d45471970624b5b3ffc2b66defab779
chenzhiheng <chenzhiheng@89trillion.com>
2021-11-05 14:50:09 +0800

    冠军联盟信息展示

---
+++ Assets/Scripts/Model/CoinModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinModel
+{
+    /// <summary>
+    /// 金币修改事件
+    /// </summary>
+    public VoidIntFunction OnCoinChallengeClick;
+
+    // 当前的金币
+    private int coin;
+
+    public CoinModel(int coin)
+    {
+        // 赋予初始值
+        this.coin = coin;
+    }
+
+    /// <summary>
+    /// 获取或修改金币
+    /// </summary>
+    public int Coin
+    {
+        get
+        {
+            return coin;
+        }
+        set
+        {
+            coin = value;
+
+            // 金币改变事件触发
+            OnCoinChallengeClick?.Invoke(this.coin);
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Event.cs
@@ -0,0 +1,5 @@
+﻿/// <summary>
+/// 当分数改变的事件
+/// </summary>
+/// <param name="score">分数</param>
+public delegate void VoidIntFunction(int score);
