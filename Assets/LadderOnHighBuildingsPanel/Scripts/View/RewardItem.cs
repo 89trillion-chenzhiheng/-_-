af0609729c7261f8b32686c9269896a81dd2d114
chenzhiheng <chenzhiheng@89trillion.com>
2021-11-08 17:27:40 +0800

    按照目录格式修改文件夹

---
+++ Assets/LadderOnHighBuildingsPanel/Scripts/View/RewardItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardItem : MonoBehaviour
+{
+    // 领取状态显示文本
+    [SerializeField] private Text receiveStateShowText;
+    // 引用天梯界面
+    [SerializeField] private LadderOnHighBuildingsPanel ladderOnHighBuildingsPanel;
+
+    // 是否被领取
+    private bool isReceive = false;
+
+    public void Awake()
+    {
+        receiveStateShowText.text = "可领取";
+    }
+
+    /// <summary>
+    /// 当奖励领取按钮被按下
+    /// </summary>
+    public void OnReceiveBtnClick()
+    {
+        // 没有被领取则进行领取后的反馈：添加金币，UI显示
+        if (!isReceive) 
+        {
+            isReceive = true;
+
+            receiveStateShowText.text = "已领取";
+
+            ladderOnHighBuildingsPanel.coinModel.Coin += 100;
+        }
+    }
+}
--- Assets/Scripts/Model/Event.cs
+++
@@ -1,5 +0,0 @@
-﻿/// <summary>
-/// 当分数改变的事件
-/// </summary>
-/// <param name="score">分数</param>
-public delegate void VoidIntFunction(int score);
