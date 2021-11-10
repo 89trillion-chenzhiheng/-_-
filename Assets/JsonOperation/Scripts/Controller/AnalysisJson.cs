6729351a2b921ca261ea43208ed67a92a3f33f1c
chenzhiheng <chenzhiheng@89trillion.com>
2021-11-10 16:42:10 +0800

    Json解析加入异常处理

--- Assets/JsonOperation/Scripts/Controller/AnalysisJson.cs
+++ Assets/JsonOperation/Scripts/Controller/AnalysisJson.cs
@@ -12,8 +12,20 @@ public class AnalysisJson
 
         // 获取数据文件
         TextAsset txt = Resources.Load<TextAsset>("JsonData") as TextAsset;
+
         // 解析文件
-        JSONNode jsonObject = JSON.Parse(txt.text);
+        JSONNode jsonObject = null;
+        try
+        {
+            jsonObject = JSON.Parse(txt.text);
+        }
+        catch (System.Exception ex)
+        {
+            // 异常处理，如果出现异常，返回null表示数据错误，没有获取到数据
+            Debug.Log(ex);
+            return null;
+        }
+
         // 将数据存入列表
         for (int i = 0; i < jsonObject[0].Count; i++)
         {
